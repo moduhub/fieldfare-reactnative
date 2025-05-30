09523e6e26f2681cf5dd4fcb0153f74e62cecce0
Adan Kvitschal <adan@moduhub.com>
2025-05-30 12:31:36 -0300

    [fix] update to complete/incomplete chunk manager

--- src/LevelUpChunkManager.js
+++ src/LevelUpChunkManager.js
@@ -4,54 +4,109 @@ import asyncstorageDown from 'asyncstorage-down'
 import levelup from 'levelup'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
+const asyncGet = (db, key) => {
+	return new Promise((resolve, reject) => {
+		db.get(key, (error, value) => {
+			if (error) {
+				reject(error);
+			} else {
+				resolve(value);
+			}
+		});
+	});
+};
 
-export class LevelUpChunkManager extends ChunkManager
-{
-	constructor()
-	{
+export class LevelUpChunkManager extends ChunkManager {
+	
+	constructor() {
 		super()
-		this.db = new levelup('./chunk', 
+		this.completeChunks = levelup('./complete-chunks', 
+		{
+			db: location => asyncstorageDown(location, { AsyncStorage })
+		})
+        this.incompleteChunks = levelup('./incomplete-chunks', 
 		{
 			db: location => asyncstorageDown(location, { AsyncStorage })
 		})
 	}
 	
-	
-	static init()
-	{
+	static init() {
 		const newInstance = new LevelUpChunkManager
 		ChunkManager.addInstance(newInstance)
 	}
 
-
-	async storeChunkContents(contents)
-	{
-		const identifier = await ChunkingUtils.generateIdentifierForData(contents)
-		await this.db.put(identifier, contents)
-		return identifier
+	async storeChunkContents(base64data) {
+		let complete = true;
+        let depth = 0;
+        let size = base64data.length;
+        if(size > 1024) {
+            throw Error('Chunk size limit exceeded');
+        }
+        const childrenIdentifiers = await ChunkingUtils.getChildrenIdentifiers(base64data);
+        for(const childIdentifier of childrenIdentifiers) {
+            let childChunk;
+            try {
+                childChunk = await asyncGet(this.completeChunks, childIdentifier);
+            } catch(error) {
+                complete = false;
+                break;
+            }
+            size += childChunk.size;
+            depth = Math.max(depth, childChunk.depth+1);
+        }
+        const identifier = await ChunkingUtils.generateIdentifierForData(base64data);
+        if(complete) {
+            await this.completeChunks.put(identifier, JSON.stringify({base64data, depth, size}));
+			console.log('LevelUpChunkManager.storeChunkContents: Stored complete chunk', identifier);
+        } else {
+            await this.incompleteChunks.put(identifier, base64data);
+			console.log('LevelUpChunkManager.storeChunkContents: Stored incomplete chunk', identifier);
+            depth = undefined;
+            size = undefined;
+        }
+        return {identifier, base64data, complete, depth, size};
 	}
 
-
-	getChunkContents(identifier)
-	{
-		// logger.debug('info', 'LevelUpChunkManager fetching res: ' + identifier)
+	getChunkContents(identifier) {
 		return new Promise((resolve, reject) => {
-			this.db.get(identifier, (error, contents) => {
-				if (error) {
-					var newError = Error('Chunk fetch failed: ' + {cause: error})
-					//logger.debug(error)
-					if (error.name === 'NotFoundError') {
-						newError.name = 'NOT_FOUND_ERROR'
+            this.completeChunks.get(identifier, (complete_error, complete_chunk_data) => {
+				if(complete_error) {
+					console.log('LevelUpChunkManager.getChunkContents: COMPLETE ERROR ' + identifier, complete_error);
+					if(complete_error.notFound) {
+						this.incompleteChunks.get(identifier, (incomplete_error, incomplete_chunk_data) => {
+							if(incomplete_error) {
+								console.log('LevelUpChunkManager.getChunkContents: INCOMPLETE ERROR ', incomplete_error);
+								if(incomplete_error.notFound) {
+									//translate to a fieldfare expected error
+									const newError = Error('Chunk not found');
+									newError.name = 'NOT_FOUND_ERROR';
+									reject(newError);
+								} else {
+									reject(incomplete_error);
+								}
+							} else {
+								console.log('LevelUpChunkManager.getChunkContents: GOT INCOMPLETE ', incomplete_chunk_data);
+								resolve({
+									base64data: incomplete_chunk_data,
+									complete:false
+								});
+							}
+						});
+					} else {
+						reject(complete_error);
 					}
-					reject(newError)
 				} else {
-					// logger.debug('Id: ' + identifier + ' contents: ' + contents)
-					resolve(contents)
+					const complete_chunk = JSON.parse(complete_chunk_data);
+					console.log('LevelUpChunkManager.getChunkContents: GOT COMPLETE xxx ', complete_chunk);
+					resolve({
+                		base64data: complete_chunk.base64data,
+                		complete: true,
+                		depth: complete_chunk.depth,
+                		size: complete_chunk.size
+            		});
 				}
-			})
-		})
-
+			});
+		});
 	}
-
 
 }
